a5986f1cfbc1b963456ad2da7be54ed2311c33a9
Elakkiya <elakkiyae264@gmail.com>
2026-04-06 11:29:39 +0530

    js updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://ai-hypertension.onrender.com";
 
 async function register(){
     const email = document.getElementById("email").value;
